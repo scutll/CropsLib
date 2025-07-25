0fa279cf6649ebebb70b82545c05469095638528
mxl <mxlscut153@163.com>
2025-07-25 20:17:59 +0800

    springboot init

---
+++ src/main/java/com/mxl/CropsLib/CropsLibApplication.java
@@ -0,0 +1,13 @@
+package com.mxl.CropsLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropsLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropsLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mxl/CropsLib/CropsLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxl.CropsLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropsLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
